45fcd12ee46aa03fb447e41f250f0897de965f19
joaoraes <moraes.santos07@escola.pr.gov.br>
2026-05-27 16:10:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
-const darkModeBtn = document.getElementById("darkModeBtn");
+const themeBtn = document.getElementById("themeBtn");
 
-darkModeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+themeBtn.addEventListener("click", () => {
 
-    if (document.body.classList.contains("dark-mode")) {
-        darkModeBtn.innerHTML = "☀️";
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.innerHTML = "☀";
     } else {
-        darkModeBtn.innerHTML = "🌙";
+        themeBtn.innerHTML = "🌙";
     }
+
 });
+
+function calcularImpacto() {
+
+    const agua = Number(document.getElementById("agua").value);
+    const arvores = Number(document.getElementById("arvores").value);
+    const reciclagem = Number(document.getElementById("reciclagem").value);
+
+    const resultado = document.getElementById("resultado");
+
+    let pontos = 0;
+
+    pontos += agua * 0.05;
+    pontos += arvores * 10;
+    pontos += reciclagem * 2;
+
+    let nivel = "";
+    let mensagem = "";
+
+    if (pontos < 50) {
+
+        nivel = "Impacto Sustentável Inicial 🌱";
+
+        mensagem =
+            "Você já possui hábitos positivos, mas ainda pode melhorar seu impacto ambiental reduzindo desperdícios e aumentando práticas sustentáveis.";
+
+    } else if (pontos < 120) {
+
+        nivel = "Impacto Sustentável Muito Bom 🌿";
+
+        mensagem =
+            "Parabéns! Suas atitudes ajudam significativamente o meio ambiente e incentivam práticas conscientes.";
+
+    } else {
+
+        nivel = "Impacto Sustentável Excelente 🌎";
+
+        mensagem =
+            "Você demonstra forte compromisso com o planeta e contribui diretamente para um futuro mais sustentável.";
+
+    }
+
+    resultado.innerHTML = `
+    <strong>${nivel}</strong><br><br>
+    Pontuação sustentável: <strong>${pontos.toFixed(0)}</strong><br><br>
+    ${mensagem}
+  `;
+
+}
 
 const counters = document.querySelectorAll(".counter");
 
